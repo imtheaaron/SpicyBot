b9695d796409c07343f1e17b4fae62a4d62bda5f
Aaron Burke <35472931+imtheaaron@users.noreply.github.com>
2018-04-09 15:22:10 -0700

    pushing files and script

---
+++ config.py
@@ -0,0 +1,6 @@
+# Twitter API Keys for Spicy Analysis SpicyAnalysis application
+
+consumer_key = "EAPf7HYFKMRzf8MivKFnNPCJ6"
+consumer_secret = "6Rxt7PFsEqXMtaYjKehmlreJls71CXIj6J5XHsUKhSaq72t26W"
+access_token = "982709303868981248-WGwSrHMpUqCeSmR3SpBH8PGumB65GGl"
+access_token_secret = "TEI5oNsJVHYB0cVXOq04whyihtUyunjwkIBiyNz8DcHI3"
